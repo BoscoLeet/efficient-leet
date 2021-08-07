d3d34c89924c082eea00efc3d73e232015253f16
BoscoLeet <787534184@qq.com>
2021-08-07 11:45:47 +0800

    Update Dijkstra.java
    完善totalWeigth函数

--- src/com/astrack/algorithm/dijkstra/Dijkstra.java
+++ src/com/astrack/algorithm/dijkstra/Dijkstra.java
@@ -12,6 +12,8 @@ public class Dijkstra {
     private Map<Integer, List<Path>> mPathMap;
 
     public Dijkstra(final int n) {
+        mNodeCount = n;
+        mVisitedNodeSet = new HashSet<>(n);
         mUnVisitedNodeSet = new HashSet<>(n);
         mWeightMap = new HashMap<>(n);
         mPathMap = new HashMap<>(n);
@@ -21,9 +23,68 @@ public class Dijkstra {
         }
     }
 
-    public int totalWeight() {
-        // 实现遍历一次的时间
-        return -1;
+    public int totalWeight(final int beginNode) {
+        mWeightMap.put(beginNode, 0);
+        int cursor = beginNode;
+        while (!mUnVisitedNodeSet.isEmpty()) {
+            if (!mPathMap.containsKey(cursor)) {
+                mVisitedNodeSet.add(cursor);
+                mUnVisitedNodeSet.remove(cursor);
+                if (mUnVisitedNodeSet.isEmpty()) {
+                    break;
+                }
+
+                int result = Integer.MAX_VALUE;
+                for (int entry : mUnVisitedNodeSet) {
+                    if (mWeightMap.get(entry) < result) {
+                        result = mWeightMap.get(entry);
+                        cursor = entry;
+                    }
+                }
+                if (result == Integer.MAX_VALUE) {
+                    return -1;
+                }
+                continue;
+            }
+
+            List<Path> pathList = mPathMap.getOrDefault(cursor, new ArrayList<>());
+            for (int i = 0, size = pathList.size(); i < size; i++) {
+                int from = pathList.get(i).mFrom;
+                int to = pathList.get(i).mTo;
+                int weight = pathList.get(i).mWeight;
+                int base = mWeightMap.get(from);
+                int nextWeight = base + weight;
+                if (nextWeight < mWeightMap.get(to)) {
+                    mWeightMap.put(to, nextWeight);
+                }
+            }
+            mVisitedNodeSet.add(cursor);
+            mUnVisitedNodeSet.remove(cursor);
+            if (mUnVisitedNodeSet.isEmpty()) {
+                break;
+            }
+
+            int result = Integer.MAX_VALUE;
+            for (int entry : mUnVisitedNodeSet) {
+                if (mWeightMap.get(entry) < result) {
+                    result = mWeightMap.get(entry);
+                    cursor = entry;
+                }
+            }
+            if (result == Integer.MAX_VALUE) {
+                return -1;
+            }
+
+        }
+
+        int result = Integer.MIN_VALUE;
+        for (Map.Entry<Integer, Integer> entry : mWeightMap.entrySet()) {
+            result = Math.max(result, entry.getValue());
+        }
+        if (result == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return result;
     }
 
     public Dijkstra withPath(final int from, final int to, final int weight) {
